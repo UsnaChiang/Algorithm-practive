c9c2589d4e556a211a801d6285999e986dffba9b
UsnaChiang <UsnaChiang@gemail.com>
2017-10-24 23:12:44 +0800

    commit20171024-1

--- src/AP20171024.java
+++ src/AP20171024.java
@@ -1,5 +1,18 @@
+
 /**
  * Created by usna on 24/10/2017.
+ * 二分查找法简单版
  */
 public class AP20171024 {
+   public static int rank(int key,int[] a){
+       int l=0;
+       int h=a.length-1;
+       while (l<h){
+           int mid= l+(h-l)/2;
+           if       (key<a[mid]) h=mid-1;
+           else   if(key>a[mid]) l=mid+1;
+           else                  return mid;
+       }
+       return -1;
+   }
 }
